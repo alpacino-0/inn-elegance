e527589fe1d64d362f6ef9ee30b907b975191904
Your Name <yavuzacenta@gmail.com>
2025-04-22 00:44:20 +0300

    Başlangıç

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import { NotFoundPage } from '@/components/404/404-page-not-found';
+
+/**
+ * Global 404 Sayfası
+ * 
+ * Bu sayfa, uygulama genelinde bir sayfa bulunamadığında görüntülenir.
+ * Kullanıcıya 404 hata sayfasını gösterir.
+ */
+export default function NotFound() {
+  return (
+    <>
+
+      <NotFoundPage />
+
+    </>
+  );
+} 
